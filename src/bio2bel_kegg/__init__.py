b20f163ecf435f906cf2f0b9fdad4fb256b35917
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2017-10-07 17:03:43 +0200

    __init__ deleted

---
+++ src/bio2bel_kegg/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.1-dev'
+
+__title__ = 'bio2bel_kegg'
+__description__ = "A package for converting Kegg to BEL"
+__url__ = 'https://github.com/bio2bel/kegg'
+
+__author__ = 'Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017 Charles Tapley Hoyt'
