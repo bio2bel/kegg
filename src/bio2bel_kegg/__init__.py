3a548544498f54bb582988cd20b6d785350c3208
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-10-30 10:28:13 +0100

    Update __init__.py

--- src/bio2bel_kegg/__init__.py
+++ src/bio2bel_kegg/__init__.py
@@ -9,5 +9,5 @@ __url__ = 'https://github.com/bio2bel/kegg'
 __author__ = 'Charles Tapley Hoyt'
 __email__ = 'charles.hoyt@scai.fraunhofer.de'
 
-__license__ = 'Apache 2.0 License'
+__license__ = 'MIT License'
 __copyright__ = 'Copyright (c) 2017 Charles Tapley Hoyt'
