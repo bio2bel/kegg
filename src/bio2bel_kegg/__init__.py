1f89db1a31bf45254ac97c1e8e477ef67213e174
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-01-01 18:32:50 +0100

    Update license

--- src/bio2bel_kegg/__init__.py
+++ src/bio2bel_kegg/__init__.py
@@ -6,8 +6,8 @@ __title__ = 'bio2bel_kegg'
 __description__ = "A package for converting Kegg to BEL"
 __url__ = 'https://github.com/bio2bel/kegg'
 
-__author__ = 'Charles Tapley Hoyt & Daniel Domingo-Fernandez'
+__author__ = 'Charles Tapley Hoyt and Daniel Domingo-Fernández'
 __email__ = 'charles.hoyt@scai.fraunhofer.de'
 
 __license__ = 'MIT License'
-__copyright__ = 'Copyright (c) 2017 Charles Tapley Hoyt'
+__copyright__ = 'Copyright (c) 2017-2018 Charles Tapley Hoyt and Daniel Domingo-Fernández'
