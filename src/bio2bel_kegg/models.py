1db8b06a88453b713edeeebba51936717e280f95
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-01-18 11:34:43 +0100

    documentation, protein links and improvements added to models

--- src/bio2bel_kegg/models.py
+++ src/bio2bel_kegg/models.py
@@ -19,8 +19,8 @@ PROTEIN_PATHWAY_TABLE = '{}_protein_pathway'.format(TABLE_PREFIX)
 protein_pathway = Table(
     PROTEIN_PATHWAY_TABLE,
     Base.metadata,
-    Column('protein_id', Integer, ForeignKey('{}.id'.format(PROTEIN_TABLE_NAME))),
-    Column('pathway_id', Integer, ForeignKey('{}.id'.format(PATHWAY_TABLE_NAME)))
+    Column('protein_id', Integer, ForeignKey('{}.id'.format(PROTEIN_TABLE_NAME)), primary_key=True),
+    Column('pathway_id', Integer, ForeignKey('{}.id'.format(PATHWAY_TABLE_NAME)), primary_key=True)
 )
 
 
@@ -31,8 +31,8 @@ class Pathway(Base):
 
     id = Column(Integer, primary_key=True)
 
-    kegg_id = Column(String(255))
-    name = Column(String(255))
+    kegg_id = Column(String(255), unique=True, nullable=False, index=True, doc='KEGG id of the pathway')
+    name = Column(String(255), doc='pathway name')
 
     proteins = relationship(
         'Protein',
@@ -62,7 +62,9 @@ class Protein(Base):
 
     id = Column(Integer, primary_key=True)
 
-    protein_id = Column(String(255))
+    kegg_id = Column(String(255), unique=True, nullable=False, index=True, doc='KEGG id of the protein')
+    uniprot_id = Column(String(255), doc='uniprot id of the protein')
+    hgnc_id = Column(String(255), doc='hgnc id of the protein')
 
     def __repr__(self):
         return self.id
@@ -74,13 +76,22 @@ class Protein(Base):
         return {
             FUNCTION: PROTEIN,
             NAMESPACE: 'UNIPROT',
-            IDENTIFIER: self.protein_id
+            IDENTIFIER: self.kegg_id
         }
 
 
 class PathwayView(ModelView):
-    column_searchable_list = [Pathway.kegg_id, Pathway.name]
+    """Pathway view in Flask-admin"""
+    column_searchable_list = (
+        Pathway.kegg_id,
+        Pathway.name
+    )
 
 
 class ProteinView(ModelView):
-    column_searchable_list = [Protein.protein_id]
+    """Protein view in Flask-admin"""
+    column_searchable_list = (
+        Protein.kegg_id,
+        Protein.uniprot_id,
+        Protein.hgnc_id
+    )
