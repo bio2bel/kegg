60aeb669c3c08f37febfac6c465e8c5e88a628ec
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-01-30 11:15:21 +0100

    Pin bio2bel_hgnc version to 0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ META_PATH = os.path.join('src', 'bio2bel_{}'.format(BIO2BEL_MODULE), '__init__.p
 INSTALL_REQUIRES = [
     'click',
     'bio2bel',
-    'bio2bel_hgnc>=0.0.2',
+    'bio2bel_hgnc>=0.0.4',
     'pybel>=0.10.1',
     'sqlalchemy==1.1.15',
     'pandas',
