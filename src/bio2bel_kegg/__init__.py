aecbf925cf84b2f81a6726ddb5894ee2b9cb37be
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-01 15:11:17 +0100

    Update paragraph

--- src/bio2bel_kegg/__init__.py
+++ src/bio2bel_kegg/__init__.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
-"""Bio2BEL KEGG is a package for enriching BEL networks with KEGG information by wrapping its RESTful API.
-Furthermore, it is integrated in the ComPath enviroment for pathway database comparison.
+"""
+Bio2BEL KEGG is a package for enriching BEL networks with KEGG information by wrapping its RESTful API.
+KEGG. This package downloads pathway information from KEGG's API and store it in template data model relating genes to pathways.
+Furthermore, it is integrated in the `ComPath environment <https://github.com/ComPath>`_ for pathway database comparison.
+
+Citation
+--------
+- Kanehisa, Furumichi, M., Tanabe, M., Sato, Y., and Morishima, K.; KEGG: new perspectives on genomes, pathways, diseases and drugs. Nucleic Acids Res. 45, D353-D361 (2017).
+- Kanehisa, M., Sato, Y., Kawashima, M., Furumichi, M., and Tanabe, M.; KEGG as a reference resource for gene and protein annotation. Nucleic Acids Res. 44, D457-D462 (2016).
+- Kanehisa, M. and Goto, S.; KEGG: Kyoto Encyclopedia of Genes and Genomes. Nucleic Acids Res. 28, 27-30 (2000).
 """
 
 from .manager import Manager
