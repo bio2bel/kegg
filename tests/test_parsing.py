9c166d6ffffa3a3fca0e2dd947d84b20057a29b9
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-01 11:23:38 +0100

    Improve test descriptions

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -51,7 +51,7 @@ class TestParse(DatabaseMixin):
         )
 
     def test_gene_query_1(self):
-        """Single protein query. This protein is associated with 3 pathways"""
+        """Single protein query. This protein is associated with 2 pathways"""
         enriched_pathways = self.manager.query_gene_set(['PFKP'])
         self.assertIsNotNone(enriched_pathways, msg='Enriching function is not working')
 
