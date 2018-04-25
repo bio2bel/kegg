a371d6f44a0cc350d137c90dff0314577a95c750
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-04-25 16:30:42 +0200

    Fix CLI

--- src/bio2bel_kegg/cli.py
+++ src/bio2bel_kegg/cli.py
@@ -7,20 +7,17 @@ import os
 import click
 from pandas import DataFrame, Series
 
-from bio2bel import build_cli
-from bio2bel_kegg.constants import DEFAULT_CACHE_CONNECTION
 from bio2bel_kegg.manager import Manager
 
 log = logging.getLogger(__name__)
 
-main = build_cli(Manager)
+main = Manager.get_cli()
 
 
 @main.command()
-@click.option('-c', '--connection', help="Defaults to {}".format(DEFAULT_CACHE_CONNECTION))
-def export(connection):
+@click.pass_obj
+def export(manager):
     """Export all pathway - gene info to a excel file"""
-    m = Manager(connection=connection)
 
     log.info("Querying the database")
 
@@ -28,7 +25,7 @@ def export(connection):
     genesets = DataFrame(
         dict([
             (k, Series(list(v)))
-            for k, v in m.export_genesets().items()
+            for k, v in manager.export_genesets().items()
         ])
     )
 
