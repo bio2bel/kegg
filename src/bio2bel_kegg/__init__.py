909e8fbd5543e5748d68ccaf34f25a66cf531e16
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-11-13 17:41:38 +0100

    Bump version: 0.2.1 → 0.2.2-dev
    [skip ci]

--- src/bio2bel_kegg/__init__.py
+++ src/bio2bel_kegg/__init__.py
@@ -18,7 +18,7 @@ Citation
 
 from .manager import Manager  # noqa: F401
 
-__version__ = '0.2.1'
+__version__ = '0.2.2-dev'
 
 __title__ = 'bio2bel_kegg'
 __description__ = "A wraper around the KEGG RESTful API"
