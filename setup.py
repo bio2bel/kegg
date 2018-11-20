3b60174afe15f3b6a9a0a60c4be9fb487438a87e
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-11-20 11:36:38 +0100

    Update minimum compath_utils version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ INSTALL_REQUIRES = [
     'click',
     'requests',
     'bio2bel>=0.2.0,<0.3.0',
-    'compath_utils>=0.1.0',
+    'compath_utils>=0.2.1',
     'bio2bel_hgnc>=0.1.0',
     'pybel>=0.12.0,<0.13.0',
     'sqlalchemy',
